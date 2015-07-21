c7faa8843e16f9b20c75433981c570a754bb978c
Micah Wood <micah@wpscholar.com>
2015-07-21 13:43:42 -0400

    Updated .gitignore and added in WordPress core

---
+++ salt.php
@@ -0,0 +1,9 @@
+<?php
+define('AUTH_KEY',         'OQWT:8pNdGMouXw[X/U!6@!0}jR)tgAA~EM_]t({Q+_/17>/O?}E1Q|i@?@Rpr|2');
+define('SECURE_AUTH_KEY',  '%TMJ$u3B8Edi-m;#R33+Wkl!+Ro2B<-Tc~OY^46O@%e(?+K1;|Mwa*pGTuY1r2-:');
+define('LOGGED_IN_KEY',    'pO(m-c$/Cj2BH}} eT5b 6ojaPiieLXoW%,T:Xweh`R[6SJ=2TpzIrC-xE<wa&/+');
+define('NONCE_KEY',        '2+mqVbGtf#Sz_vrBtoA _lAb,g1L~RKfrKKJ`{Gt4v&[ef5`4u&iHf6^Z>!xZ3WF');
+define('AUTH_SALT',        'X]$bcX}@J;O->;OVMNX(`~~dnC@D?4hw,0M5+v<+xhwqm9e1&*jhG^*+I}OjL}ds');
+define('SECURE_AUTH_SALT', 'gumJq(;<TC[nh.R<Xc|ZkBHYx8MxYtEWd?33;Wf>-P?wqh1BHvW>f~)%bqi;-m3[');
+define('LOGGED_IN_SALT',   'P#hFZ4(#|6$$N 3i&DzWY7na<Z4=rv>h~)0BEXtP+u-k6?[d-^C7[k|nCLD1HO;n');
+define('NONCE_SALT',       '3icH$<lXL&+kQ+I9{J%bpxvqN#`GNBx@;+ggUd4#MaE[xa@IM28|k-z&*Gix7``%');
